9d73871b65c108339298329339845cb992262532
mrsterner <simon@sterner.dev>
2021-10-13 17:19:12 +0200

    Flashlight with shaders instead

--- src/main/java/dev/mrsterner/phasmophobia/PhasmophobiaClient.java
+++ src/main/java/dev/mrsterner/phasmophobia/PhasmophobiaClient.java
@@ -2,13 +2,25 @@ package dev.mrsterner.phasmophobia;
 
 import dev.mrsterner.phasmophobia.client.renderer.PlaceableBlockEntityRenderer;
 import dev.mrsterner.phasmophobia.common.registry.PhasmoObjects;
+import ladysnake.satin.api.event.ShaderEffectRenderCallback;
+import ladysnake.satin.api.managed.ManagedShaderEffect;
+import ladysnake.satin.api.managed.ShaderEffectManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.minecraft.util.Identifier;
 
 public class PhasmophobiaClient implements ClientModInitializer {
+    private static final ManagedShaderEffect GREYSCALE_SHADER = ShaderEffectManager.getInstance()
+        .manage(new Identifier(Phasmophobia.MODID, "shaders/post/transparency.json"));
+    private static boolean enabled = true;  // can be disabled whenever you want
 
     @Override
     public void onInitializeClient() {
         BlockEntityRendererRegistry.INSTANCE.register(PhasmoObjects.PLACEABLE_BLOCK_ENTITY, ctx -> new PlaceableBlockEntityRenderer());
+        ShaderEffectRenderCallback.EVENT.register(tickDelta -> {
+            if (enabled) {
+                GREYSCALE_SHADER.render(tickDelta);
+            }
+        });
     }
 }
