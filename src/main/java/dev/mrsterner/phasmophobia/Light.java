b164407a8709e044526faa32b2c67ef06fb33a4d
mrsterner <simon@sterner.dev>
2021-10-13 03:18:00 +0200

    Flashlight hits entities too

--- src/main/java/dev/mrsterner/phasmophobia/Light.java
+++ src/main/java/dev/mrsterner/phasmophobia/Light.java
@@ -1,9 +1,12 @@
 package dev.mrsterner.phasmophobia;
 
 import dev.mrsterner.phasmophobia.common.registry.PhasmoObjects;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.ActionResult;
@@ -13,9 +16,8 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
 public interface Light {
@@ -38,35 +40,47 @@ public interface Light {
         }
         return new TypedActionResult<ItemStack>(ActionResult.PASS, playerIn.getStackInHand(handIn));
     }
-    default BlockHitResult rayTrace(double blockReachDistance, float partialTicks, PlayerEntity player) {
-        float playerPitch = player.getPitch();
-        float playerYaw = player.getYaw();
-        float playerYawCos = MathHelper.cos(-playerYaw * 0.017453292F - 3.1415927F);
-        float playerYawSin = MathHelper.sin(-playerYaw * 0.017453292F - 3.1415927F);
-        float playerPitchCos = -MathHelper.cos(-playerPitch * 0.017453292F);
-        float playerPitchSin = MathHelper.sin(-playerPitch * 0.017453292F);
-        float playerNormal1 = playerYawSin * playerPitchCos;
-        float playerNormal2 = playerYawCos * playerPitchCos;
-        Vec3d vec3d = player.getCameraPosVec(partialTicks).add((double) playerNormal1 * blockReachDistance, (double) playerPitchSin * blockReachDistance, (double) playerNormal2 * blockReachDistance);
-        return player.world.raycast(new RaycastContext(player.getCameraPosVec(partialTicks), vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, player));
+
+    default HitResult getRayTraceTarget(Entity entity, World world, double reach, boolean ignoreEntities, boolean ignoreLiquids) {
+        HitResult crosshairTarget = null;
+        if (entity != null && world != null) {
+            float td = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT ? MinecraftClient.getInstance().getTickDelta() : 1f;
+            crosshairTarget = entity.raycast(reach, td, !ignoreLiquids);
+            if (!ignoreEntities) {
+                Vec3d vec3d = entity.getCameraPosVec(td);
+                double e = reach;
+                e *= e;
+                if (crosshairTarget != null)
+                    e = crosshairTarget.getPos().squaredDistanceTo(vec3d);
+                Vec3d vec3d2 = entity.getRotationVec(td);
+                Vec3d vec3d3 = vec3d.add(vec3d2.x * reach, vec3d2.y * reach, vec3d2.z * reach);
+                Box box = entity.getBoundingBox().stretch(vec3d2.multiply(reach)).expand(1.0D, 1.0D, 1.0D);
+                EntityHitResult entityHitResult = ProjectileUtil.raycast(entity, vec3d, vec3d3, box, (entityx) -> !entityx.isSpectator() && entityx.collides(), e);
+                if (entityHitResult != null) crosshairTarget = entityHitResult;
+            }
+        }
+        return crosshairTarget;
     }
 
     default void createLight(ItemStack itemStack, World world, PlayerEntity player) {
         if (!world.isClient) {
             if (itemStack.getNbt().getInt("on") == 1) return;
-
-            HitResult lookPos = rayTrace(40, 1.0f, player);
-            BlockPos pos = null;
-            if(lookPos.getType() == HitResult.Type.MISS) return;
-            else if(lookPos.getType() == HitResult.Type.ENTITY){
-                EntityHitResult entityHit = (EntityHitResult) lookPos;
-                Entity entity = entityHit.getEntity();
-                pos = entity.getBlockPos();
-                System.out.println("EntityPos: "+pos);
-            }else if(lookPos.getType() == HitResult.Type.BLOCK){
-                BlockHitResult blockHit = (BlockHitResult) lookPos;
-                pos = blockHit.getBlockPos().add(((BlockHitResult) lookPos).getSide().getVector());
-                System.out.println("BlockPos: "+pos);
+            HitResult lookPos = getRayTraceTarget(player, world, 30, false, false);
+            BlockPos pos;
+            switch (lookPos.getType()) {
+                case MISS -> {return;}
+                case BLOCK -> {
+                    BlockHitResult blockHit = (BlockHitResult) lookPos;
+                    pos = blockHit.getBlockPos().add(((BlockHitResult) lookPos).getSide().getVector());
+                    System.out.println("BlockPos: "+pos);
+                }
+                case ENTITY -> {
+                    EntityHitResult entityHit = (EntityHitResult) lookPos;
+                    Entity entity = entityHit.getEntity();
+                    pos = entity.getBlockPos();
+                    System.out.println("EntityPos: "+pos);
+                }
+                default -> pos = ((BlockHitResult) lookPos).getBlockPos();
             }
             double vecDistance = Math.pow(lookPos.getPos().squaredDistanceTo(player.getPos()), 0.5);
             if (vecDistance <= 40) {
